7ad01b7a138e74f7c5926522e9de253252e2aa53
Mohamed_Alburky <albrkyhmady3@gmail.com>
2026-07-16 13:19:52 +0200

    Switch email port to 465 SSL to bypass Render outbound blocks for the 3 time

--- server.ts
+++ server.ts
@@ -579,12 +579,16 @@ async function sendPasswordResetOTPEmail(toEmail: string, userName: string, code
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: gmailUser,
-        pass: gmailPass,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // استخدام منفذ 587 مع بروتوكول STARTTLS
+  auth: {
+    user: gmailUser,
+    pass: gmailPass,
+  },
+  family: 4
+ 
+}as SMTPTransport.Options);
 
     const subject = "🔐 رمز التحقق لإعادة تعيين كلمة المرور - نظام الدعم الفني لمجموعة خليفة القابضة";
 
@@ -631,16 +635,14 @@ async function sendLoginAlertEmail(toEmail: string, userName: string, isActivati
   try {
      const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // استخدام منفذ 587 مع بروتوكول STARTTLS
+  port: 465,
+  secure: true, // استخدام منفذ 587 مع بروتوكول STARTTLS
   auth: {
     user: gmailUser,
     pass: gmailPass,
   },
-  tls: {
-    rejectUnauthorized: false // لمنع حظر الاتصال بسبب شهادات الأمان المحلية
-  }
-});
+  family: 4
+  }as SMTPTransport.Options);
 
     const subject = isActivation 
       ? "🔐 تم تفعيل حسابك بنجاح - نظام الدعم الفني لمجموعة خليفة القابضة" 
